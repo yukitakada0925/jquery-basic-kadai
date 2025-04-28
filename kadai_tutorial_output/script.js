1d43da11dd8b40bec57973419a2ff65c0caa9f83
yukitakada0925 <lovepopcorn000925@gmail.com>
2025-04-28 16:01:36 +0900

    Add files via upload

--- kadai_tutorial_output/script.js
+++ kadai_tutorial_output/script.js
@@ -28,6 +28,18 @@ $(function () {
   
     // 入力チェックをした結果をresultに格納
     let result = inputCheck();
+    // エラー判定とメッセージを取得
+     let error = result.error;
+     let message = result.message;
+
+         // エラーが無かったらフォームを送信する
+    if (error == false) {
+      // フォーム送信は実際には行わず、送信成功メッセージのみ表示する
+      alert('お問い合わせを送信しました。')
+    } else {
+      // エラーメッセージを表示する
+      alert(message);
+    }
   });
 
   // フォーカスが外れたとき（blur）にフォームの入力チェックをする
@@ -50,8 +62,64 @@ $(function () {
     inputCheck();
   });
 
-      // お問い合わせフォームの入力チェック
   function inputCheck() {
-    console.log('inputCheck関数の呼び出し');
+    // エラーのチェック結果
+    let result;
+
+    // エラーメッセージのテキスト
+    let message = '';
+
+    // エラーがなければfalse、エラーがあればtrue
+    let error = false;
+
+    // お名前のチェック
+     if ($('#name').val() == '') {
+    // エラーあり
+    $('#name').css('background-color', '#f79999');
+    error = true;
+    message += 'お名前を入力してください。\n';
+     } else {
+    // エラーなし
+    $('#name').css('background-color', '#fafafa');
+    }
+   // メールアドレスのチェック
+   if ($('#email').val() == '' || $('#email').val().indexOf('@') == -1 || $('#email').val().indexOf('.') == -1) {
+    // エラーあり
+    $('#email').css('background-color', '#f79999');
+    error = true;
+     message += 'メールアドレスが未記入、または「@」「.」が含まれていません。\n';
+   } else {
+    // エラーなし
+   $('#email').css('background-color', '#fafafa');
+   }
+   // 電話番号のチェック（未入力はOK、未入力でない場合は-が必要）
+   if ($('#tel').val() != '' && $('#tel').val().indexOf('-') == -1) {
+      // エラーあり
+       $('#tel').css('background-color', '#f79999');
+       error = true;
+       message += '電話番号に「-」が含まれていません。\n';
+      } else {
+      // エラーなし
+        $('#tel').css('background-color', '#fafafa');
+     }
+         // 個人情報のチェックボックスのチェック
+    if ($('#agree').prop('checked') == false) {
+      error = true;
+      message += '個人情報の取り扱いについてご同意いただける場合は、チェックボックスにチェックしてください。\n';
+    }
+       // エラーの有無で送信ボタンを切り替え
+    if (error == true) {
+      $('#submit').attr('src', 'images/button-submit.png');
+     } else {
+       $('#submit').attr('src', 'images/button-submit-blue.png');
+    }
+       // オブジェクトでエラー判定とメッセージを返す
+       result = {
+        error: error,
+        message: message
+      }
+  
+      // 戻り値としてエラーがあるかどうかを返す
+      return result;
   }
  });
